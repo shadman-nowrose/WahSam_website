d8e5ad6fbc236f0993896589144e223ca9f0fa46
Shadman Nowose <113465524+shadman-nowrose@users.noreply.github.com>
2023-06-05 23:00:14 +0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,156 @@
+import product1 from './Images/product1.jpg';
+import product2 from './Images/product2.jpg';
+
+const productsData = [
+  {
+    id: 1,
+    title: 'HR Coil',
+    image: product1,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 1,
+  },
+  {
+    id: 2,
+    title: 'HR & CRCA Furniture',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 1,
+  },
+  {
+    id: 3,
+    title: 'PPGI & GP Sheet',
+    image: product1,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 2,
+  },
+  {
+    id: 4,
+    title: 'Checkered Plate',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+  {
+    id: 5,
+    title: 'MS Box Pipe',
+    image: product1,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 2,
+  },
+  {
+    id: 6,
+    title: 'Industrial Storage Rack',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 7,
+    title: 'Electrical Switch & Boards',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 8,
+    title: 'Circuit Breaker',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 9,
+    title: 'Industrial Air Conditioner',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 10,
+    title: 'Distribution Board',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 11,
+    title: 'Commericial Air Conditioner',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 12,
+    title: 'Industrial Tiles/Marble',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 13,
+    title: 'Paints/Coatings',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 14,
+    title: 'Food Supplement',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 15,
+    title: 'Biotechnologies',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 16,
+    title: 'API',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 17,
+    title: 'Excipients',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 18,
+    title: 'Coating & Color',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 19,
+    title: 'Fish Oil',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+    {
+    id: 20,
+    title: 'Distillers Dried Grains with Soluble',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  },
+  {
+    id: 21,
+    title: 'Soya Cake',
+    image: product2,
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    categoryId: 3,
+  }
+
+  // Add more products here...
+];
+
+export default productsData;
